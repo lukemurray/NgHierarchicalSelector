28dae99e490c99df8c72fc2fc6fb6d6244792859
Luke Murray <lukemurray@gmail.com>
2014-12-08 21:04:40 +1100

    first commit, semi-working tree selector for angular

---
+++ demo/demo.js
@@ -0,0 +1,49 @@
+angular.module('demo', ['hierarchical-selector'])
+
+.controller('DemoCtrl', function($scope) {
+  $scope.data1 = [];
+
+  for (var i = 0; i < 7; i++) {
+    var obj = {
+      id: i,
+      name: 'Node ' + i,
+      children: []
+    };
+
+    for (var j = 0; j < 3; j++) {
+      var obj2 = {
+        id: j,
+        name: 'Node ' + i + '.' + j,
+        children: []
+      };
+      obj.children.push(obj2);
+    }
+
+    $scope.data1.push(obj);
+  }
+
+  $scope.data1[1].children[0].children.push({
+    id: j,
+    name: 'Node sub_sub 1',
+    children: []
+  });
+
+  $scope.data2 = angular.copy($scope.data1);
+  $scope.data3 = angular.copy($scope.data1);
+  $scope.data4 = angular.copy($scope.data1);
+
+  $scope.onSelectionChanged = function(items, str) {
+    str = '';
+    if (items) {
+      for (var i = 0; i < items.length; i++) {
+        str += items[i].name + ', ';
+      }
+    }
+  };
+
+  $scope.selectOnly1Or2 = function(item) {
+    return /[12]/.test(item.name);
+  };
+})
+
+;
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+var templateCache = require('gulp-angular-templatecache');
+var del = require('del');
+
+var _outputDir = 'build';
+var _htmlWatchPaths = ['src/**/*.html'];
+var _jsAppWatchPaths = ['src/**/*.js'];
+var _lessAppWatchPaths = ['src/**/*.less'];
+var _tplAppWatchPaths = ['src/**/*.tpl.html'];
+
+gulp.task('js', function() {
+  gulp.src('src/hierarchical-selector.js')
+  .pipe(gulp.dest(_outputDir))
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+// compile LESS and lint css
+gulp.task('less', function() {
+  return gulp.src('src/hierarchical-selector.less')
+  .pipe(less({
+    //paths: [ path.join(__dirname) ]
+  }))
+  .pipe(gulp.dest(_outputDir));
+});
+
+gulp.task('templates', function () {
+  gulp.src('src/**/*.tpl.html')
+  .pipe(templateCache('hierarchical-selector.templates.js', {module: 'hierarchical-selector'}))
+  .pipe(gulp.dest(_outputDir));
+});
+
+// build the client for devleopment (watches and recompiles etc.)
+gulp.task('build', ['js', 'less', 'templates']);
+
+// Build the client and server
+gulp.task('default', ['build'])
+
+// Build the client and server and start the server
+gulp.task('watch', ['build'], function() {
+  gulp.watch(_jsAppWatchPaths, ['js']);
+  gulp.watch(_lessAppWatchPaths, ['less']);
+  gulp.watch(_tplAppWatchPaths, ['templates']);
+});
+
+gulp.task('clean', function(cb) {
+  del([_outputDir], cb);
+});
