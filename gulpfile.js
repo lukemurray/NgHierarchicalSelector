71ce40694b080b0d09d55b9a99947bbbd30431a2
Luke Murray <lukemurray@gmail.com>
2014-12-10 22:02:50 +1100

    version

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
+var fs = require('fs');
+
 var gulp = require('gulp');
 
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var rename = require("gulp-rename");
 var jshint = require('gulp-jshint');
+var bump = require('gulp-bump');
 var templateCache = require('gulp-angular-templatecache');
 var del = require('del');
 var karma = require('karma').server;
@@ -21,7 +28,7 @@ gulp.task('js', function() {
 
 // compile LESS and lint css
 gulp.task('less', function() {
-  return gulp.src('src/hierarchical-selector.less')
+  gulp.src('src/hierarchical-selector.less')
   .pipe(less({
     //paths: [ path.join(__dirname) ]
   }))
@@ -51,8 +58,37 @@ gulp.task('tdd', function (done) {
 // build the client for devleopment (watches and recompiles etc.)
 gulp.task('build', ['js', 'less', 'templates']);
 
+gulp.task('bump-minor', function(){
+  gulp.src('./package.json')
+    .pipe(bump({type:'minor'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-patch', function(){
+  gulp.src('./package.json')
+    .pipe(bump({type:'patch'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('min-css', function() {
+  var version = JSON.parse(fs.readFileSync('./package.json').toString()).version;
+  gulp.src(_outputDir + '/*.css')
+    .pipe(minifyCSS())
+    .pipe(rename('ng-hierarchical-celector.' + version + '.min.css'))
+    .pipe(gulp.dest(_outputDir));
+});
+
+gulp.task('min', ['min-css'], function() {
+  var version = JSON.parse(fs.readFileSync('./package.json').toString()).version;
+  gulp.src(_outputDir + '/*.js')
+    .pipe(concat(_outputDir + '/ng-hierarchical-celector.' + version + '.js'))
+    .pipe(uglify())
+    .pipe(rename('ng-hierarchical-celector.' + version + '.min.js'))
+    .pipe(gulp.dest(_outputDir));
+});
+
 // Build the client and server
-gulp.task('default', ['build'])
+gulp.task('default', ['clean', 'build'])
 
 // Build the client and server and start the server
 gulp.task('watch', ['build'], function() {
